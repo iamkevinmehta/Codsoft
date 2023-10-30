bafb1ab92f3b3ae4709c22028c9f231a371ea453
Kevin Mehta <78220203+iamkevinmehta@users.noreply.github.com>
2023-10-30 23:44:14 +0530

    Task 3
    Codsoft Java Internship

---
+++ AtmInterface.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+        }
+    }
+}
+
+class ATM {
+    private BankAccount account;
+
+    public ATM(BankAccount account) {
+        this.account = account;
+    }
+
+    public void displayOptions() {
+        System.out.println("ATM Options:");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            displayOptions();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Current Balance: " + account.getBalance());
+                    break;
+                case 2:
+                    System.out.print("Enter the deposit amount: ");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+                case 3:
+                    System.out.print("Enter the withdrawal amount: ");
+                    double withdrawalAmount = scanner.nextDouble();
+                    account.withdraw(withdrawalAmount);
+                    break;
+                case 4:
+                    System.out.println("Exiting ATM. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please choose a valid option.");
+            }
+        } while (choice != 4);
+
+        scanner.close();
+    }
+}
+
+public class AtmInterface {
+    public static void main(String[] args) {
+        // Create a bank account with an initial balance of $1000
+        BankAccount bankAccount = new BankAccount(1000);
+
+        // Initialize and run the ATM with the bank account
+        ATM atm = new ATM(bankAccount);
+        atm.run();
+    }
+}
